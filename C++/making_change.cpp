fb7f014e5ceae116bf653fdc2f4e572188667253
Rajiuddin <skrajiuddin18@gmail.com>
2024-10-10 19:01:55 +0530

    Reset

---
+++ C++/making_change.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    bool validchange(vector<int>& num){
+        
+    }
+};
+
+int main(){
+    Solution ob;
+    vector<int> num={5,5,10,15};
+    cout<<(ob.validchange(num) ? "True" : "False")<<endl;
+}
